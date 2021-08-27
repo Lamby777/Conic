b193e460e35ca2f676f452a85cec84df7490b5c7
Dexie <35111165+Lamby777@users.noreply.github.com>
2021-08-27 16:37:42 +0000

    bruh 2: electric boogaloo

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const fs = require("fs")
+let code;
 
 try {
-	const data = fs.readFileSync('code.con', 'utf8');
-	console.log(data);
+	code = fs.readFileSync('code.con', 'utf8');
 } catch (err) {
+	console.error("There was a problem loading code.");
 	console.error(err);
+}
+
+lexer(code);
+
+/* Steps:
+* First, take all literals and put them in an array
+* Before running, search the code for forbidden lexer phrases
+* such as _ or *
+* Replace the literals in the original string with the reserved phrase "lit"
+* Then, evaluate the missing things and replace the originals with the new stuff.
+*/
+
+function lexer() {
+	//
+	if (code.contains("_") || code.contains("*"))
+		throw Error("Lexer phrases must be removed from code");
+	let final = [];
+	return final;
 }
