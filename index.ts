ee595f3a34493f9bbe7e2d3043b872efd9bd55b8
Dexie <35111165+Lamby777@users.noreply.github.com>
2022-01-08 09:38:05 +0000

    Ohm semantic actions

--- index.ts
+++ index.ts
@@ -17,7 +17,43 @@ try {
 }
 
 let semantics = grammar.createSemantics().addOperation("run", {
-	Expression(val) {return val},
+	_iter(...children) {return children.map(val => val.run())},
+
+	// Statements
+	Statement_Call(varval, _eol) {
+		//
+	},
+
+	Statement_if(_if, boolcheck, ifblock, _elseif, _elif,
+				 boolcheck2, elifblock, _else, elseblock) {
+		//
+	},
+
+	
+	// Declarations
+	VariableD(vtype, vname, _equal, newval, _eol) {
+		//
+	},
+	
+	FunctionD(rtype, fname, _open, params, _close, code, _eol) {
+		//
+	},
+
+	
+	// Literals
+	numLiteral(_) {return parseFloat(this.sourceString)},
+	comment(_) {},
 });
 
-code;
+execute(code);
+
+
+
+function execute(code: string) {
+	let matched = grammar.match(code);
+	
+	if (matched.failed())
+		throw SyntaxError("Compiler error: " + matched);
+	else
+		semantics(matched).run();
+}
